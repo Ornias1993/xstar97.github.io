bf983a9fd65b8dbad373737a8525312a2c43865e
xstar97 <dev.xstar97@gmail.com>
2023-02-05 23:49:45 -0500

    add ChartsSearchBar

---
+++ src/components/ChartsSearchBar.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+
+const ChartsSearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [trains, setTrains] = useState([]);
+  const [filteredTrains, setFilteredTrains] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('/charts/charts.json');
+      const json = await res.json();
+      setTrains(json);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const results = trains.filter(train => {
+      return train.charts.some(chart => {
+        return (
+          chart.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          chart.description.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+      });
+    });
+    setFilteredTrains(results);
+  }, [searchTerm, trains]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search apps by name or description"
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
+      {searchTerm && (
+        <ul>
+          {filteredTrains.map(train => (
+            <li key={train.name}>
+              <h3>{train.name}</h3>
+              {train.charts.map(chart => (
+                <div key={chart.name}>
+                  <h4><a href={chart.link}>{chart.name}</a></h4>
+                  <p>{chart.description}</p>
+                </div>
+              ))}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ChartsSearchBar;
