9ea115bc4648e4f459b83fc3f97b702f69eaa6b3
xstar97 <dev.xstar97@gmail.com>
2023-02-06 02:49:03 -0500

    update search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,19 +2,17 @@ import React, { useState } from "react";
 
 const SearchBar = ({ placeHolder,searchTerm, handleSearch }) => {
   return (
-    <div className="search-container">
-      <label htmlFor="search-input">
-        <i className="fa fa-search"></i>
-      </label>
-      <input
-        id="search-input"
-        type="text"
-        placeholder={placeHolder}
-        value={searchTerm}
-        onChange={handleSearch}
-        style={{width: '100%'}}
-      />
-    </div>
+    <div style={{display: 'flex', alignItems: 'center'}}>
+      <img src="/img/truecharts.png" style={{marginRight: '0.5em'}} width="24" height="24"/>
+  <input
+    id="search-input"
+    type="text"
+    placeholder={placeHolder}
+    value={searchTerm}
+    onChange={handleSearch}
+    style={{flexGrow: 1}}
+  />
+</div>
   );
 };
 
