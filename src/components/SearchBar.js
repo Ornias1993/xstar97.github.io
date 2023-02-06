e2ff4b3cb403dca4f5cf0b8c929d43483dda43cd
xstar97 <dev.xstar97@gmail.com>
2023-02-06 01:45:21 -0500

    import css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-
 import "/css/searchbar.css";
+import React, { useState } from "react";
 
 const SearchBar = ({ searchTerm, handleSearch }) => {
   return (
