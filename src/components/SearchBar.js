2d48ccbd62b0dceac99179c50429cce6d4ec88e5
xstar97 <dev.xstar97@gmail.com>
2023-02-06 02:17:29 -0500

    update component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,21 @@
 //import "../css/searchbar.css";
 import React, { useState } from "react";
 
-const SearchBar = ({ placeholder,searchTerm, handleSearch }) => {
+const SearchBar = ({ placeholder, searchTerm, handleSearch }) => {
   return (
-    <div style={{ display: "flex" }}>
-      <span style={{ marginRight: "10px" }}>
+    <div style={{ display: "flex", width: "100%" }}>
+      <span style={{ marginRight: "10px", alignSelf: "center" }}>
         <i className="fa fa-search" aria-hidden="true" />
       </span>
       <input
+        style={{ width: "100%" }}
         type="text"
         placeholder={placeholder}
         value={searchTerm}
         onChange={(event) => handleSearch(event.target.value)}
       />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
