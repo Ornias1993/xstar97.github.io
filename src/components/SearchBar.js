33fe963563d2024dbc8a933bbf0586c7d3177632
xstar97 <dev.xstar97@gmail.com>
2023-02-06 03:16:36 -0500

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,7 @@ const SearchBar = ({ placeHolder,searchTerm, handleSearch }) => {
   return (
     <div style={{display: 'flex', alignItems: 'center'}}>
       <img src="/img/truecharts.png" style={{marginRight: '0.5em'}} width="24" height="24"/>
-      <input id="search-input" type="text" placeholder={placeHolder} value={searchTerm} onChange={handleSearch} style={{flexGrow: 1, height: '40px'}} />
+      <input id="search-input" type="text" placeholder={placeHolder} value={searchTerm} onChange={handleSearch} style={{flexGrow: 1, height: '40px', paddingLeft: '10px'}} />
 </div>
   );
 };
